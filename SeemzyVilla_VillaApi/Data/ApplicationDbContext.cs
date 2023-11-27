828016ac0efcf112c81c623559fe2fdac45035c8
wassem7 <waseemdarkwa@gmail.com>
2023-11-27 11:30:10 +0000

    Connection String

---
+++ SeemzyVilla_VillaApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeemzyVilla_VillaApi.Models;
+
+namespace SeemzyVilla_VillaApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- SeemzyVilla_VillaApi/Models/Villa.cs
+++ SeemzyVilla_VillaApi/Models/Villa.cs
@@ -1,7 +1,12 @@
-﻿namespace SeemzyVilla_VillaApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SeemzyVilla_VillaApi.Models
 {
     public class Villa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
